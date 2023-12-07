93c5b1a0792a098ebe55723581c584ec0a6c54de
vicsnet <adesanmiadebisi@gmail.com>
2023-12-07 14:46:25 +0100

    test started

---
+++ src/SoulBound.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+import 'lib/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol';
+
+contract SoulBound is ERC1155{
+address owner;
+
+uint256 AllowTransfer = 1;
+uint256 DontTransfer = 2;
+
+mapping (address=>uint256) myToken;
+
+constructor()ERC1155(""){
+owner = msg.sender;
+}
+
+/**
+* @notice Token are required to be claimed by all user before they can mint token, receive token or transfer token 
+ */
+function claimToken(uint256 _myToken) public returns(uint256){
+
+        myToken[msg.sender] = _myToken;
+        return _myToken;
+
+}
+/**
+*@notice function to set the uri
+ */
+function setURI(string memory _uri) external{
+        _setURI(_uri);
+}
+
+/**
+@notice function to mint token
+ */
+function mintToken() public{
+        require(myToken[msg.sender] >0, 'GO_AND_CLAIM_TOKEN');
+        uint256 id = myToken[msg.sender];
+        uint256 value;
+         if (id % 2 == 0) {
+            value= 1e18;
+        } else {
+            value = 1e27;
+        }
+        _mint(msg.sender, id , value, '');
+}
+ 
+/**
+@notice The original safeTraansfer From was overriten
+*/ 
+     function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public override {
+        revert();
+        // super.safeTransferFrom(from, to, id, amount, data);
+    }
+
+/**
+*@notice function to transfer token
+*/
+  function transfer(address to) external  {
+        require(myToken[msg.sender] >0, 'GO_AND_CLAIM_TOKEN');
+        require(myToken[to]>0, 'NO_TOKEN_CLAIMED_BY_USER_YET');
+          uint256 id = myToken[msg.sender];
+          require(id %2 == 0, 'YOU_CAN_NOT_TRANSFER');
+          require(myToken[to] %2 == 0, 'YOU_CAN_NOT_TRANSFER_TO_THE_ADDRESS');
+
+     
+           _safeTransferFrom(msg.sender, to, id, 1e18, '');
+   
+    }
+}
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -3,22 +3,41 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
+import {SoulBound} from '../src/SoulBound.sol';
 
-contract CounterTest is Test {
+contract SoulBoundTest is Test {
     Counter public counter;
+    SoulBound public soulbound;
+
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+     
+        soulbound = new SoulBound();
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testClaimToken() public{
+        soulbound.claimToken(1);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testSetURI() public{
+        soulbound.setURI('advabnssmss');
     }
+
+    function testMintToken() public{
+        testSetURI();
+        testClaimToken();
+        
+
+        soulbound.mintToken();
+    }
+
+    // function test_Increment() public {
+    //     counter.increment();
+    //     assertEq(counter.number(), 1);
+    // }
+
+    // function testFuzz_SetNumber(uint256 x) public {
+    //     counter.setNumber(x);
+    //     assertEq(counter.number(), x);
+    // }
 }
